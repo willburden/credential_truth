7a8246c4c7f84e75017eb7b9797d2ee009efb648
Will Burden <25230174+willburden@users.noreply.github.com>
2021-11-19 05:47:20 +0000

    Update install.sh to use latest release

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-wget https://github.com/willburden/credential_truth/releases/download/v0.1/docker-credential-truth -O /tmp/docker-credential-truth
+wget https://github.com/willburden/credential_truth/releases/latest/download/docker-credential-truth -O /tmp/docker-credential-truth
 chmod +x /tmp/docker-credential-truth
 mv /tmp/docker-credential-truth /usr/bin
