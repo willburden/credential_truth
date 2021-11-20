34842948ec82f10c4b8a114e3e63b9819912e8fe
Will Burden <25230174+willburden@users.noreply.github.com>
2021-11-20 17:03:23 +0000

    Improve install.sh output

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-wget https://github.com/willburden/credential_truth/releases/latest/download/docker-credential-truth -O /tmp/docker-credential-truth
-chmod +x /tmp/docker-credential-truth
-mv /tmp/docker-credential-truth /usr/bin
+echo "Downloading..."
+wget https://github.com/willburden/credential_truth/releases/latest/download/docker-credential-truth -O /tmp/docker-credential-truth 2> /dev/null
+echo "Installing..."
+chmod +x /tmp/docker-credential-truth > /dev/null
+mv -f /tmp/docker-credential-truth /usr/bin > /dev/null
+echo "Finished."
